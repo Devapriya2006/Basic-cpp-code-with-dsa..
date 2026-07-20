5bf5b4497e7f3fc3be47a45b45b76b4875431216
DEVAPRIYA PAUL KUNDU <devapriya2006paul@gmail.com>
2026-07-21 00:20:44 +0530

    Add basic C++ program to append to a string

---
+++ cpp.42.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    string str="HELLO";
+    // cout<<str.length(); // use to find length
+    str.append(" c++"); // HELLO C++
+    return 0;
+}
